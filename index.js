f0a95032153133ad623df3a7fc4b2491caacf81d
yli0607x <github email address>
2018-11-21 09:54:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+   const node = document.getElementById('grand-node');
+   const child = node.querySelectorAll('div');
+   return child[child.length-1];
+}
+
+function increaseRankBy(n) {
+    const ranked = document.getElementsByClassName('ranked-list')
+    for (let i = 0; i < ranked.length; i++) {
+      ranked[i].innerHTML = (parseInt(i) + 1).toString();
+    }
+}
